1cde82c500ca06a0aec1c199496661ce2b218f4f
Geison Amorim <geisonam43@gmail.com>
2022-04-20 20:36:46 -0300

    Add project files.

---
+++ RetanguloClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo R = new Retangulo();
+
+            Console.WriteLine("Entre  com os dados do retângulo: ");
+            Console.Write("Largura: ");
+            R.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            R.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(R);
+        }
+    }
+}
---
+++ RetanguloClass/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura * 2) + (Altura * 2);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+
+        public override string ToString()
+        {
+            return "Area = " + Area().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPerimetro = "
+                + Perimetro().ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDiagonal = "
+                + Diagonal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
